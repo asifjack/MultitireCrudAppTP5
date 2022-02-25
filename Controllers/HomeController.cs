b578c8e04175cad7923710db28e706ce6f18508c
asifjack <mdasifwr@gmail.com>
2022-02-25 11:10:03 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using BussinessAceessLayer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MultitireCrudAppTP5.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultitireCrudAppTP5.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private BLEmployeeBussiness bussiness = new BLEmployeeBussiness();
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+
+        }
+
+        public IActionResult Index()
+        {
+            var emp = bussiness.GetEmployees();
+            return View(emp);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
